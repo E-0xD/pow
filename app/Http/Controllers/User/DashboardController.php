18a29a4f3dcff2c4f7b5bc6b40513fee1dbb70ae
E-0xD <danielewere99@gmail.com>
2025-10-31 01:41:29 +0100

    organised dashboard view, made total portfolio count display

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -12,6 +13,7 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('user.dashboard');
+       $totalPortfolio =  Auth::user()->portfolios()->count();
+        return view('user.dashboard.index', compact('totalPortfolio'));
     }
 }
