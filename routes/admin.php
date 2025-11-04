a88368cb24cb3c7d3f1eea46a4dc807a32fd952b
E-0xD <danielewere99@gmail.com>
2025-11-04 11:51:19 +0100

    made /metric th eindex route  /

--- routes/admin.php
+++ routes/admin.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::domain('admin.' . parse_url(config('app.url'), PHP_URL_HOST))->name('admin.')->middleware(['auth', 'ensure.admin', 'ensure.active'])->group(function () {
     Route::resource('template', TemplateController::class);
     Route::resource('user', UserController::class)->except(['edit']);
-    Route::get('metrics', [MetricsController::class, 'index'])->name('metrics.index');
+    Route::get('/', [MetricsController::class, 'index'])->name('metrics.index');
 
     // Affiliate Management
     Route::resource('affiliate', AffiliateController::class)->except(['create', 'show', 'store']);
