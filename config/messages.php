8ad601982408840a28dc73cb22da57af570749b0
E-0xD <danielewere99@gmail.com>
2025-11-10 18:26:56 +0100

    back to default

--- config/messages.php
+++ config/messages.php
@@ -10,10 +10,6 @@ return [
                 'Our goal is to help you stand out and open new doors of opportunity. If you have ideas or features you’d love to see, we’d love to hear from you anytime.',
                 'Here’s to building something meaningful; and to landing that next big, life-changing opportunity.'
             ],
-
-            'actionText' => 'Go to Dashboard',
-            'actionUrl'  => route('user.dashboard'),
-            'outroLines' => ['If you need help, reply to this email.'],
         ],
         'subject' => 'Your Work, Your Badge. let’s make it count.'
     ],
@@ -22,13 +18,10 @@ return [
         'payload' => [
             'greeting' => 'Welcome back',
             'introLines' => [
-                'We noticed a new login to your account on ' . now()->format('Y-m-d H:i:s') . ' UTC ',
+                'We noticed a new login to your account on',
                 'It’s great to see you again. keep growing your digital proof of work and showcasing what you do best.',
                 'If this wasn’t you, please visit your account settings right away to secure your profile.'
             ],
-            'actionText' => 'Go to Dashboard',
-            'actionUrl'  => route('user.dashboard'),
-            'outroLines' => ['If you need help, reply to this email.'],
         ],
         'subject' => 'Login Attempt',
 
