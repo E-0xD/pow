e2ed19c3eb023d31728faa0da06dd596c0d85540
E-0xD <danielewere99@gmail.com>
2025-12-15 09:58:14 +0100

    route fixing

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -62,10 +62,10 @@
                     <a @class([
                         'flex items-center gap-3 px-3 py-2 rounded-lg',
                         'dark:bg-primary/20 text-primary dark:text-white bg-primary/10' => request()->routeIs(
-                            'admin.user.*'),
+                            'admin.metrics.index'),
                         'hover:bg-gray-100 dark:hover:bg-white/10 text-[#1F2937] dark:text-gray-300' => !request()->routeIs(
-                            'admin.user.*'),
-                    ]) href="{{ route('user.dashboard') }}">
+                            'admin.metrics.index'),
+                    ]) href="{{ route('admin.metrics.index') }}">
                         <span class="material-symbols-outlined text-2xl">admin_panel_settings</span>
                         <p class="text-sm font-medium leading-normal">Admin</p>
                     </a>
