3eb253c6b944998d22702fe3e02628b34546922f
E-0xD <danielewere99@gmail.com>
2026-01-21 04:36:25 +0100

    Remove DeletePendingPortfolios command and its scheduled task

--- routes/console.php
+++ routes/console.php
@@ -5,6 +5,5 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::exec(base_path('queue-monitor.sh'))->everyMinute();
-Schedule::command('portfolio:delete-pending')->dailyAt('02:00');
 Schedule::command('subscriptions:process-expired')->everySixHours();
 Schedule::command('auth:clear-resets')->everyFifteenMinutes();
